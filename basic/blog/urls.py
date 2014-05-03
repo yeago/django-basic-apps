adbe3bb58e3811ad1f775ad3f7cbff0cc2890664
yeago <subsume@gmail.com>
2014-05-03 21:31:33 +0200

    consider site

--- basic/blog/urls.py
+++ basic/blog/urls.py
@@ -4,23 +4,26 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from basic.blog.models import Category, Post
 from basic.blog.views import PostListCategory
+from django.conf import settings
+
+qs = Post.objects.filter(site=settings.SITE_ID)
 
 
 urlpatterns = patterns('basic.blog.views',
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
-        DetailView.as_view(model=Post),
+        DetailView.as_view(queryset=qs),
         name='blog_detail'
     ),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/$',
-        dates.DayArchiveView.as_view(allow_future=False, date_field='publish', model=Post),
+        dates.DayArchiveView.as_view(allow_future=False, date_field='publish', queryset=qs),
         name='blog_archive_day'
     ),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
-        dates.MonthArchiveView.as_view(allow_future=False, date_field='publish', model=Post),
+        dates.MonthArchiveView.as_view(allow_future=False, date_field='publish', queryset=qs),
         name='blog_archive_month'
     ),
     url(r'^(?P<year>\d{4})/$',
-        dates.YearArchiveView.as_view(allow_future=False, date_field='publish', model=Post),
+        dates.YearArchiveView.as_view(allow_future=False, date_field='publish', queryset=qs),
         name='blog_archive_year'
     ),
     url(r'^categories/(?P<slug>[-\w]+)/$',
@@ -39,7 +42,7 @@ urlpatterns = patterns('basic.blog.views',
     #    name='blog_index_paginated'
     #),
     url(r'^$',
-        dates.ArchiveIndexView.as_view(model=Post, paginate_by=20, date_field='publish', allow_future=False),
+        dates.ArchiveIndexView.as_view(queryset=qs, paginate_by=20, date_field='publish', allow_future=False),
         name='blog_index',
     ),
 )
