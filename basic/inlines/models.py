fe55aedb43a8b29aca917998c0e76cfc0f95e9d3
yeago <yeago999@gmail.com>
2020-01-27 20:05:26 +0000

    on delete

--- basic/inlines/models.py
+++ basic/inlines/models.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 class InlineType(models.Model):
     """InlineType model"""
     title = models.CharField(max_length=200)
-    content_type = models.ForeignKey(ContentType)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'inline_types'
