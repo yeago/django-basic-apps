d0f4f6d347f55b2dbcd1538fb2a7fcd9c285b791
steve yeago <subsume@gmail.com>
2020-01-29 01:58:17 -0500

    Update feeds.py

--- basic/blog/feeds.py
+++ basic/blog/feeds.py
@@ -4,7 +4,7 @@ from django.contrib.sites.models import Site
 from django.contrib.syndication.feeds import Feed
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.comments.models import Comment
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from basic.blog.models import Post, Category
 
 
